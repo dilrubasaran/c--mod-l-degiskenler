d074e825967d71555e2c384c58c54537eaebadde
DİLRUBA <tyryt30@gmail.com>
2022-03-02 16:07:53 +0300

    dfl

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace degisken
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("");
+            byte b = 5;    //1byte
+            sbyte c = 5;   //1byte
+
+            short s = 5;    //2byte 
+        }
+    }
+}
