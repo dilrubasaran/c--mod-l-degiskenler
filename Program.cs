6059c9ff9d9eea69a05d8fe425aad4da44a9546b
DİLRUBA <tyryt30@gmail.com>
2022-03-03 12:23:51 +0300

    commit degiskenler

--- Program.cs
+++ Program.cs
@@ -8,10 +8,23 @@ namespace degisken
         {
         
             Console.WriteLine("");
-            byte b = 5;    //1byte
-            sbyte c = 5;   //1byte
 
-            short s = 5;    //2byte 
+            
+            string str20 = "20";
+            int int20 = 20;
+            string yenideğer = str20 + int20.ToString();
+            Console.WriteLine(yenideğer);
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21);
+            
+            int int22 = int20 + int.Parse(str20);            
+            string datetime = DateTime.Now.ToString("dd.mm.yyyy");
+            Console.WriteLine(datetime);
+            string hour = DateTime.Now.ToString("hh.mm");
+            Console.WriteLine(hour);
+
         }
+
     }
 }
